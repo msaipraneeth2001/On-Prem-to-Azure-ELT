e3b9b87459bdaaa2aebbe45399b8796fbea7a9c0
M Sai Praneeth <72293235+msaipraneeth2001@users.noreply.github.com>
2024-08-31 23:41:05 +0530

    Add files via upload

---
+++ SQL Script/sp_CreateSQLServerlessView_gold.sql
@@ -0,0 +1,22 @@
+    USE gold_db
+    GO
+
+    CREATE OR ALTER PROC CreateSQLServerlessView_gold @ViewName NVARCHAR(100)
+    AS
+    BEGIN
+
+    DECLARE @statement VARCHAR(MAX)
+
+        SET @statement = N'CREATE OR ALTER VIEW ' + @ViewName +'  AS
+            SELECT *
+            FROM
+                OPENROWSET(
+                    BULK ''https://dedatalake2024.dfs.core.windows.net/gold/SalesLT/' + @ViewName + '/'',
+                    FORMAT = ''DELTA''
+                ) as [result]
+        '
+
+    EXEC (@statement)
+
+    END
+    GO
